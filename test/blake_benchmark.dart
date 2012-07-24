b9744e086ccf440fe8cd8ecfbda6171547e0b4c1
Dmitry Chestnykh <dmitry@codingrobots.com>
2012-07-24 14:33:56 +0200

    Add benchmark.

---
+++ test/blake_benchmark.dart
@@ -0,0 +1,31 @@
+#import("dart:crypto");
+#import("../blake.dart");
+
+final MEGABYTE = 1024*1024;
+
+measure(name, fn) {
+  var stopWatch = new Stopwatch.start();
+  var bytes = fn();
+  print("${name} ${stopWatch.elapsedInMs()/1000}s per ${bytes/MEGABYTE} MB");
+}
+
+main() {
+  print("Running...");
+  
+  var zeroes = [];
+  zeroes.insertRange(0, MEGABYTE, 0);
+  
+  measure("BLAKE-256", () {
+    var blake = new BLAKE256();
+    blake.update(zeroes);
+    blake.digest();
+    return zeroes.length;
+  });
+  
+  measure("SHA-256", () {
+    var sha = new SHA256();
+    sha.update(zeroes);
+    sha.digest();
+    return zeroes.length;
+  });
+}
